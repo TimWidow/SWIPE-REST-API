8320e3332e39e32c7aed783e65060ef0cd4af140
TimWidow <eat.twix@gmail.com>
2021-11-20 11:05:12 +0200

    deploy docker requirements.txt

--- swipe/settings.py
+++ swipe/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'admin',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
