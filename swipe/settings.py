e61109fdcf1d4d1ed7879e1a8f949ad313676149
TimWidow <eat.twix@gmail.com>
2021-11-20 11:18:52 +0200

    deploy docker requirements.txt

--- swipe/settings.py
+++ swipe/settings.py
@@ -106,10 +106,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
